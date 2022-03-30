0594e6a7fa2de5a870d974ffc3707e046743908b
Mayowa Oluwasina <mayowaoluwasina@gmail.com>
2022-03-30 09:08:16 +0100

    ignore node modules

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -62,7 +62,7 @@ router.post(
       status: order.status,
       userId: order.userId,
       expiresAt: order.expiresAt.toISOString(),
-      version: 8483848,
+      version: order.version,
       ticket: {
         id: ticket.id,
         price: ticket.price,
